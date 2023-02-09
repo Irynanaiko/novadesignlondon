488655ebc3045cdcebaef73a00c15f1a80c9f732
Akr1s <dimakr71@gmail.com>
2023-02-09 22:18:52 +0200

    update form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,44 +2,48 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './Form.scss';
+import { Stack } from 'react-bootstrap';
 
 function Forms() {
     return (
-        <div className='form'>
-            <div className='form-container'>
-                <Form className='form-fields'>
-                    <h3 className='form-title'>request a quote</h3>
+        <Stack
+            direction="horizontal"
+            className="justify-content-center align-items-center gap-sm-3 gap-lg-5 f-container"
+        >
+            <div className="f-info bg-color">WE COVER ALL AREAS IN AND AROUND GREATER LONDON</div>
+            <Form className="form">
+                <h3 className="form-title">request a quote</h3>
 
-                    <Form.Group className="mb-3 p-0 form-control-sm  opacity-50" controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Name" />
-                    </Form.Group>
+                <Form.Group className="mb-3 p-0 form-control-sm" controlId="formBasicEmail">
+                    <Form.Control type="text" placeholder="Name" className="opacity-75" />
+                </Form.Group>
 
-                    <Form.Group className="mb-3 p-0 form-control-sm opacity-50" controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Email" />
-                    </Form.Group>
+                <Form.Group className="mb-3 p-0 form-control-sm" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Email" className="opacity-75" />
+                </Form.Group>
 
-                    <Form.Group className="mb-3 p-0 form-control-sm  opacity-50" controlId="formBasicEmail">
-                        <Form.Control type="number" placeholder="Phone" />
-                    </Form.Group>
+                <Form.Group className="mb-3 p-0 form-control-sm" controlId="formBasicEmail">
+                    <Form.Control type="number" placeholder="Phone" className="opacity-75" />
+                </Form.Group>
 
-                    <Form.Group className="mb-3 p-0 form-control-sm opacity-50" controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Address" />
-                    </Form.Group>
+                <Form.Group className="mb-3 p-0 form-control-sm" controlId="formBasicEmail">
+                    <Form.Control type="text" placeholder="Address" className="opacity-75" />
+                </Form.Group>
 
-                    <Form.Group className="mb-3 p-0 form-control-sm opacity-50" controlId="formBasicEmail">
-                        <Form.Control as="textarea" type="text" placeholder="About project" />
-                    </Form.Group>
-                    <Button className='w-100 text-uppercase' variant="dark">Submit</Button>
-                </Form>
-            </div>
-
-            <div className='f-text'>
-                WE COVER ALL AREAS IN AND AROUND GREATER LONDON
-            </div>
-
-        </div>
-
-    )
+                <Form.Group className="mb-3 p-0 form-control-sm" controlId="formBasicEmail">
+                    <Form.Control
+                        as="textarea"
+                        type="text"
+                        placeholder="About project"
+                        className="opacity-75"
+                    />
+                </Form.Group>
+                <Button className="w-100 text-uppercase" variant="dark">
+                    Submit
+                </Button>
+            </Form>
+        </Stack>
+    );
 }
 
 export { Forms };
