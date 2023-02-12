a1802a13c6c50cdd2cde553bad8435e9520f6810
Akr1s <dimakr71@gmail.com>
2023-02-12 22:26:00 +0200

    update footer adaptivity

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,53 +9,78 @@ import InstagramIcon from '../icons/InstagramIcon';
 import FacebookIcon from '../icons/FacebookIcon';
 import { navLinks } from '../../constants/links';
 
-const iconStyle = { width: '30px', height: '30px', color: 'white' };
-
 function Footer() {
     return (
         <div className="footer">
             <Container fluid="xl">
                 <Row>
-                    <Col sm={3}>
+                    <Col xs={6} lg={3} className="order-2 order-lg-1">
                         <Stack gap={3} className="p-2">
-                            <p className="text-center fs-2 text-white">Contact us:</p>
-                            <Stack direction="horizontal" gap={2}>
-                                <EnvelopeIcon style={iconStyle} />
+                            <p className="text-center text-white footer-section-title">
+                                Contact us:
+                            </p>
+                            <Stack
+                                direction="horizontal"
+                                gap={2}
+                                className="justify-content-center justify-content-lg-start"
+                            >
+                                {/* TODO this svg scales when screen size changes, needs
+                                investigation */}
+                                <EnvelopeIcon className="footer-icon" />
                                 <Link
                                     to="mailto:novadesignslondon@gmail.com"
-                                    className="text-decoration-none text-white"
+                                    className="text-decoration-none text-white footer-section-text"
                                 >
                                     novadesignslondon@gmail.com
                                 </Link>
                             </Stack>
-                            <Stack direction="horizontal" gap={2}>
-                                <PhoneIcon style={iconStyle} />
+                            <Stack
+                                direction="horizontal"
+                                gap={2}
+                                className="justify-content-center justify-content-lg-start"
+                            >
+                                <PhoneIcon className="footer-icon" />
                                 <Link
                                     to="tel:+447843303975"
-                                    className="text-decoration-none text-white"
+                                    className="text-decoration-none text-white footer-section-text"
                                 >
                                     +447843303975
                                 </Link>
                             </Stack>
                         </Stack>
                     </Col>
-                    <Col sm={6}>
-                        <Nav className="w-100 h-100 justify-content-center align-items-center gap-sm-1 gap-xl-3 flex-nowrap">
+                    <Col xs={12} lg={6} className="order-1 order-lg-2">
+                        <Nav className="w-100 h-100 justify-content-center align-items-center gap-sm-1 gap-xl-3 flex-nowrap footer-nav">
                             {navLinks.map(({ title, link }) => (
-                                <Nav.Link as={Link} to={link} className="text-white">
+                                <Nav.Link
+                                    as={Link}
+                                    to={link}
+                                    className="text-white"
+                                    style={{ fontSize: '16px' }}
+                                >
                                     {title}
                                 </Nav.Link>
                             ))}
                         </Nav>
                     </Col>
-                    <Col sm={3}>
+                    <Col xs={6} lg={3} className="order-3 order-lg-3">
                         <Stack gap={3} className="p-2">
-                            <p className="text-center fs-2 text-white">Following us:</p>
-                            <Stack direction="horizontal" gap={2}>
-                                <InstagramIcon style={iconStyle} />
+                            <p className="text-center text-white footer-section-title">
+                                Following us:
+                            </p>
+                            <Stack
+                                direction="horizontal"
+                                gap={2}
+                                className="justify-content-center justify-content-lg-start"
+                            >
+                                <InstagramIcon className="footer-icon" />
                             </Stack>
-                            <Stack direction="horizontal" gap={2}>
-                                <FacebookIcon style={iconStyle} />
+                            <Stack
+                                direction="horizontal"
+                                gap={2}
+                                className="justify-content-center justify-content-lg-start"
+                            >
+                                <FacebookIcon className="footer-icon" />
                             </Stack>
                         </Stack>
                     </Col>
